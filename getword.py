32d0bcd327ca18ad6e235889205d8372770fde9f
ZixiangLiu <zixiangliu2@gmail.com>
2017-04-23 14:10:14 -0400

    Updated Comments

--- getword.py
+++ getword.py
@@ -5,10 +5,12 @@
 import feedparser
 # import numpy as np
 
-#
-# Used to parse a long string of Html and recognize all the words in it
-# count the total appearance time of each word and store in dictionary list_in
-#
+'''
+Used to parse a long string of Html and recognize all the words in it
+count the total appearance time of each word and store in dictionary list_in
+input:	str_in: input string that need to be analyzed, will be splited by space
+	list_in:a dictionary, store the frequency, key is the word, value is the frequency in that blog
+'''
 def getwords(str_in, list_in, dropwords):
 	record = False # initialize the recording situation
 	string = "" # initialize string
@@ -25,9 +27,13 @@ def getwords(str_in, list_in, dropwords):
 			if i.isalpha() or i ==' ' or i == '\'': # only record the letters and space and '
 				string += i
 
-# 
-# function used to count the times of appearance of a word in the dictionary
-# 
+'''
+function used to count the times of appearance of a word in the dictionary
+input:	someword:	input word that need to be added
+	list_in:	a dictionary, store the frequency, key is the word, value is the frequency in that blog
+	dropwords: 	a list of words that user wish to drop from the input
+update: dropwords are added to the function
+'''
 def addwordtodic(someword, list_in, dropwords=[]):
 	word = someword.lower()
 	if word in list_in.keys(): # if the dictionary has the key, count
@@ -47,11 +53,12 @@ with dropwordfile as filestream:
 		dropwords.append(line.lower()[:-1])
 
 
-# 
-# The main functioning part
-# input a file, read all the urls, open and count their words
-# output to a txt
-# 
+'''
+The main functioning part
+input a file, read all the urls, open and count their words
+output to a txt
+'''
+
 all_dictionary = [] # initialize dictionary, a list of dictionaries
 
 filename = input("\nDefault file feedlist.txt\nPress Enter to select default file.\nEnter the file name: ") or "feedlist.txt"
@@ -71,7 +78,11 @@ with url as filestream:
 			all_dictionary.append(one_dictionary) # add the current dictionary to the list
 url.close()
 
-
+'''
+Combine a list of dictionaries into a large dictionary
+input:	all_dictionary:		a list of dictionaries of each blog
+output:	large_dictionary:	one dicionaries that has word as the key, and  a list of frequencies of that word in each blog
+'''
 def combinedictionary(all_dictionary):
 	large_dictionary = {}
 	length = len(all_dictionary)
@@ -90,14 +101,21 @@ large_dictionary = combinedictionary(all_dictionary)
 # Output to a file
 # 
 outputfile= open("output.txt", "w")
-
+'''
+this function write one word and its frequencies in each blog in rows
+and frequencies of each word in one blog in columns
+'''
 def rowWordcolTimes(outputfile, large_dictionary):
 	for word, time in large_dictionary.items():
 		outputfile.write("{:<30}".format(word))
 		for i in time:
 			outputfile.write("{:>5}".format(i))
 		outputfile.write('\n')
-
+'''
+this function write one word and its frequencies in each blog in columns
+and frequencies of each word in one blog in rows
+a transpose of the previous function
+'''
 def rowTimescolWord(outputfile, large_dictionary):
 	for word in large_dictionary:
 		outputfile.write("{:<30}".format(word))
