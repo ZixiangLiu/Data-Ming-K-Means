31ac51ef976b3c11fc9e4a8a328da79b15a072d6
ZixiangLiu <zixiangliu2@gmail.com>
2017-04-30 20:37:07 -0400

    correction on last upload
    last upload was using the wrong file, this upload corrects it

--- kmean.py
+++ kmean.py
@@ -13,6 +13,10 @@ import time
 This program read a output file that each row is a list of number of appearance in one blog
 then use k-means to calculate it clusters
 '''
+num_cluster_default = 4
+wordstart_default = 0
+wordend_default = 100
+same_k_repeat = 10
 
 # print("Press Enter to select default.")
 # num_cluster = input("Please enter K(default {}):".format(num_cluster_default)) or num_cluster_default
@@ -20,40 +24,30 @@ then use k-means to calculate it clusters
 # wordend = input("Please enter word end index(default {}):".format(wordend_default)) or wordend_default
 # filename = input("\nDefault file output.txt\nPress Enter to select default file.\nEnter the file name: ") or "output.txt"
 
-def get_file_words(filename, cal_range = False, wordstart = 0, wordend = 100):
-	'''
-	get words into list from file
+num_cluster = num_cluster_default
+wordstart = wordstart_default
+wordend = wordend_default
+filename = "output.txt"
 
-	Args:
-		filename (string): name of the file from which we get words
-		cal_range (boolean): True to get words from a start and an end, False to get all words
-		wordstart (int): the index of word where we start reading
-		wordend (int): the index of word where we end reading
-	Return:
-		wordlist (list of string): the words
-		bloglist (list of list of float): the frequency of words in each blog
-	'''
-	inputfile = open(filename, "r")
-	wordlist = []
-	bloglist = []
-	with inputfile as filestream:
-		first = True
-		for line in filestream:
-			linelist = line.split();
-			if first:
-				for word in linelist:
-					wordlist.append(word)
-				first = False
-			else:
-				blog = []
-				if cal_range:
-					for count in linelist[wordstart:wordend]:
-						blog.append(float(count))
-				else:
-					for count in linelist:
-						blog.append(float(count))
-				bloglist.append(blog)
-	return wordlist, bloglist
+inputfile = open(filename, "r")
+wordlist = []
+bloglist = []
+with inputfile as filestream:
+	first = True
+	for line in filestream:
+		linelist = line.split();
+		if first:
+			for word in linelist:
+				wordlist.append(word)
+			first = False
+		else:
+			blog = []
+			for count in linelist[wordstart:wordend]:
+				blog.append(float(count))
+			bloglist.append(blog)
+
+# print(len(wordlist))
+# print(len(bloglist[0]))
 
 def blog_dist(blog1, blog2):
 	'''
@@ -226,7 +220,7 @@ def init_cluster(num_cluster):
 	return clusters
 
 
-def kmeanpp_cal(num_cluster, inputlist, dist, max_iter = 300, debugging = False):
+def kmean_cal(num_cluster, inputlist, dist, max_iter = 300, debugging = False):
 	'''
 	calculate the k-means cluster
 	Args:
@@ -247,7 +241,7 @@ def kmeanpp_cal(num_cluster, inputlist, dist, max_iter = 300, debugging = False)
 	# each pivot is an list
 	start = time.time()
 	pivots = centers(inputlist, num_cluster, dist)
-	centert = time.time()-start
+	duration = time.time()-start
 	distsum = 0
 
 	# clusters is a dictionary of clusters, key pivot index, value is a list of input list index
@@ -255,7 +249,7 @@ def kmeanpp_cal(num_cluster, inputlist, dist, max_iter = 300, debugging = False)
 
 	old_clusters = {}
 	num_of_iter = max_iter
-	start = time.time()
+
 	for i in range(0, max_iter):
 		# if debugging:
 		# 	print("Iteration No.{}".format(i))
@@ -296,195 +290,125 @@ def kmeanpp_cal(num_cluster, inputlist, dist, max_iter = 300, debugging = False)
 		# if debugging:
 		# 	print("End of iteration No.{}\n\n".format(i))
 		distsum = distsumer(pivots, inputlist, clusters)
-	calt = time.time()-start
-	return clusters, pivots, num_of_iter, distsum, centert, calt
+	return clusters, pivots, num_of_iter, distsum, duration
 
-
-def kmean_cal(num_cluster, inputlist, dist, max_iter = 300, debugging = False):
-	'''
-	calculate the k-means cluster
-	similar to perious method, but not using K-means ++
-
-	Args:
-		num_cluster (int): number of clusters
-		inputlist (list of list of floats): list of smaller lists, each smaller list is a blog, containning numbers as frequencies
-		dist (function with args(list of floats, list of floats)): function to calculate distance between two blogs
-		max_iter (int): max times of iteration before abortion, default 300
-		debugging (boolean): default False, true to print info while running
-	Return:
-		group (dicionary): key is pivots index, value is list of inputlist indexes in that cluster
-		pivots (list of list of floats): centers of the K-means algorithm
-		num_of_iter (int): total number of iteration to get the output
-		distsum (float): sum of each blog's distance to its corresponding center of the result
-		centert (float): time used in calculating centers
-		calt (float): time used in calculating result
-	'''
-	length = len(inputlist)
-	# each pivot is an list
-	start = time.time()
-	pivots = [inputlist[i] for i in random.sample(range(0, length), num_cluster)]
-	distsum = 0
-
-	# clusters is a dictionary of clusters, key pivot index, value is a list of input list index
-	clusters = init_cluster(num_cluster)
-
-	old_clusters = {}
-	num_of_iter = max_iter
-	
-	for i in range(0, max_iter):
-		# if debugging:
-		# 	print("Iteration No.{}".format(i))
-		for k in range(0, length):
-			min_dist = sys.maxsize
-			min_index = 0
-			for j in range(0, num_cluster):
-				distance = dist(inputlist[k], pivots[j])
-				if distance < min_dist:
-					min_dist = distance
-					min_index = j
-			clusters[min_index].append(k)
-			# if debugging:
-			# 	print("blog No.{} is in cluster No.{}".format(k, min_index))
-
-		# condition of convergence, if new iteration did not change items in cluster
-		if clusters == old_clusters:
-			num_of_iter = i
-			distsum = distsumer(pivots, inputlist, clusters)
-			# if debugging:
-			# 	print("k=",num_cluster, "iter No.", i, "does not change cluster, end of iteration\n")
-			break;
-
-		for k in range(0, num_cluster):
-			list_len = len(pivots[0])
-			count_matrix = []
-			for j in clusters[k]:
-				count_matrix.append(inputlist[j])
-			# if debugging:
-			# 	print("k=",num_cluster, "iter No.", i, "cluster No.", k, "has member", len(count_matrix))
-			# unless the cluster is empty
-			if clusters[k]:
-				pivots[k] = [sum(np.array(count_matrix)[:, j])/float(len(clusters[k])) for j in range(0, list_len)]
-			# save the current result in a seperate dictionary
-			old_clusters[k] = [j for j in clusters[k]]
-			clusters[k] = []
-
-		# if debugging:
-		# 	print("End of iteration No.{}\n\n".format(i))
-		distsum = distsumer(pivots, inputlist, clusters)
-	calt = time.time()-start
-	return clusters, pivots, num_of_iter, distsum, calt
-
-def write_group(file, groups):
-	length = len(groups)
-	file.write("K = {}:\n".format(length))
-	# for i in range(0, length):
-	# 	file.write("{:3d} ".format(i))
-	# 	for j in groups[i]:
-	# 		file.write("{:3d}".format(j))
-	# 	file.write("\n")
-	for i in range(0, length):
-		file.write("{:3d} {}\n".format(i, groups[i]))
-	file.write("\n\n")
-
-
-def run_kmean(same_k_repeat, klist, write_group_file = True, write_time_file = True):
-	'''
-	run kmean several times and report result
-
-	Args: 
-		same_k_repeat (int): the times of repeat of same k 
-		klist (list of int): list of k values that need to calculate
-		write_group_file (boolean): true to write group information to a file
-		write_time_file (boolean): true to write time information to a file
-	Return:
-		output (list of outputs of k_means algorithms): best outputs of K_means for each k
-		outputpp (list of outputs of k_means++ algorithms): best outputs of K_means++ for each k
-		distsumlist (list of min distance result of k_means algorithms): best distance sum of K_means for each k
-		distsumlistpp (list of min distance result of k_means++ algorithms): best distance sum of K_means++ for each k
-	'''
-	output = []
-	outputpp = []
-	distsumlist = []
-	distsumlistpp = []
-	kppiter = []
-	kiter = []
-	centt = []
-	calt = []
-	kmeant = []
-	if write_group_file:
-		group_result = open("groups.txt", "w")
-	for num_cluster in klist:
-		start = time.time()
-		outputtemp = []
-		outputmin = [0, 0, 0, sys.maxsize]
-		outputminpp = [0, 0, 0, sys.maxsize]
-
-		for i in range(0,same_k_repeat): 
-			outputtemppp = kmeanpp_cal(num_cluster, bloglist, blog_dist, debugging = False)
-			if outputtemppp[3] < outputminpp[3]:
-				outputminpp = outputtemppp
-
-			outputtemp = kmean_cal(num_cluster, bloglist, blog_dist, debugging = False)
-			if outputtemp[3] < outputmin[3]:
-				outputmin = outputtemp
-		
-		outputpp.append(outputminpp)
-		output.append(outputmin)
-
-		if write_time_file:
-			centt.append(outputminpp[4])
-			calt.append(outputminpp[5])
-			kppiter.append(outputminpp[2])
-			kmeant.append(outputmin[4])
-			kiter.append(outputmin[2])
-
-
-		if write_group_file:
-			group_result.write("K-means ++ ")
-			write_group(group_result, outputminpp[0])
-			group_result.write("K-means ")
-			write_group(group_result, outputmin[0])
-
-		distsumlistpp.append(outputminpp[3])
-		distsumlist.append(outputmin[3])
-
-	if write_group_file:
-		group_result.close()
-
-	if write_time_file:
-		timefile= open("time_word{}_repeat{}_iterate.txt".format(wordend-wordstart, same_k_repeat), "w")
-		for i in [j-1 for j in klist]:
-			timefile.write("{:10} {:10.5f}\n".format(i, myt[i]))
-		for i in range(0, len(centt)):
-			timefile.write("{:10.5f} {:10.5f} {:10.5f} {:10.5f} {:10.5f} {:10.5f}\n".format(centt[i], calt[i], centt[i]+calt[i], kmeant[i], kppiter[i], kiter[i]))
-		timefile.close()
-	return output, outputpp, distsumlist, distsumlistpp
-
-
-'''
-initial parameters
-default things
-'''
-num_cluster_default = 4
-wordstart_default = 0
-wordend_default = 9500
-same_k_repeat = 10
-cal_range = False
-klist = range(1, len(bloglist)+1)
-
-wordlist, bloglist =  get_file_words("output.txt")
 bloglist = tfidfs(tf(bloglist))
 
-temp = run_kmean(same_k_repeat, klist)
+'''
+block to run repeatedly 5 times
+'''
+
+# for l in range(0, 5):
+# 	start = time.time()
+# 	output = []
+# 	klist = range(1, len(bloglist))
+# 	distsumlist = []
+# 	for num_cluster in klist:
+# 		outputtemp = []
+# 		outputmin = [0, 0, 0, sys.maxsize]
+# 		for i in range(0,same_k_repeat): 
+# 			outputtemp = kmean_cal(num_cluster, bloglist, blog_dist, debugging = False)
+# 			if outputtemp[3] < outputmin[3]:
+# 				outputmin = outputtemp
+# 		distsumlist.append(outputmin[3])
+# 	end = time.time()
+# 	myt = end-start
+# 	print("My function runs {:.5f}s".format(myt))
+
+# 	packageoutput = []
+# 	start = time.time()
+# 	for num_cluster in klist:
+# 		kmeans = KMeans(n_clusters=num_cluster).fit(bloglist)
+# 		packageoutput.append(kmeans.inertia_)
+# 	end = time.time()
+# 	pkt = end-start
+# 	print("Sklearn function runs {:.5f}s".format(pkt))
+	# outputfile= open("time_word{}_repeat{}.txt".format(wordend-wordstart, same_k_repeat), "a")
+	# outputfile.write("{:.5f} {:.5f}\n".format(myt, pkt))
+	# outputfile.close()
+
+''' to run k from 1 to total length '''
+
+output = []
+klist = range(1, len(bloglist)+1)
+myt = []
+distsumlist = []
+center_times = []
+for num_cluster in klist:
+	start = time.time()
+	outputtemp = []
+	outputmin = [0, 0, 0, sys.maxsize]
+	for i in range(0,same_k_repeat): 
+		outputtemp = kmean_cal(num_cluster, bloglist, blog_dist, debugging = False)
+		if outputtemp[3] < outputmin[3]:
+			outputmin = outputtemp
+			center_times.append(outputtemp[4])
+	output.append(outputmin)
+	distsumlist.append(outputmin[3])
+	end = time.time()
+	myt.append(end-start)
+
+packageoutput = []
+pkt = []
+
+for num_cluster in klist:
+	start = time.time()
+	kmeans = KMeans(n_clusters=num_cluster).fit(bloglist)
+	packageoutput.append(kmeans.inertia_)
+	end = time.time()
+	pkt.append(end-start)
+
+outputfile= open("time_word{}_repeat{}_iterate.txt".format(wordend-wordstart, same_k_repeat), "a")
+efficency = []
+for i in [j-1 for j in klist]:
+	outputfile.write("{:10} {:10.5f} {:10.5f} {:10.5f} {:10.5f}%\n".format(i, myt[i], pkt[i], myt[i]/pkt[i], (myt[i]-pkt[i])/pkt[i]*100))
+	efficency.append(myt[i]/pkt[i])
+outputfile.close()
+
+plt.plot(klist, efficency)
+plt.title("repeat {} times for each K, my Kmean time divided by package time".format(same_k_repeat))
+plt.xlabel("k")
+plt.ylabel("ratio")
+plt.show()
+
+''' to run single k'''
+# start = time.time()
+# output = []
+# num_cluster = 4
+# outputtemp = []
+# outputmin = [0, 0, 0, sys.maxsize]
+# for i in range(0,same_k_repeat): 
+# 	outputtemp = kmean_cal(num_cluster, bloglist, blog_dist, debugging = False)
+# 	if outputtemp[3] < outputmin[3]:
+# 		outputmin = outputtemp
+# outputmin[3] is min distance
+# end = time.time()
+# myt = end-start
+# print("My function runs {:.5f}s".format(myt))
+# packageoutput = []
+# start = time.time()
+
+# kmeans = KMeans(n_clusters=num_cluster).fit(bloglist)
+# packageoutput.append(kmeans.inertia_)
+
+# end = time.time()
+# pkt = end-start
+# print("Sklearn function runs {:.5f}s".format(pkt))
+
+# outputfile= open("time_word{}_repeat{}_single{}.txt".format(wordend-wordstart, same_k_repeat, num_cluster), "a")
+# outputfile.write("{:10.5f} {:10.5f} {:10.5f} {:10.5f}%\n".format(myt, pkt, myt/pkt, (myt-pkt)/pkt*100))
+# outputfile.close()
 
 
-
-
-
-
-
-
-
-
-
+# plt.plot(klist, distsumlist, label="My function")
+# plt.plot(klist, packageoutput, label = "Sklearn.KMeans")
+# plt.title("repeat {} times for each K".format(same_k_repeat))
+# plt.xlabel("k")
+# plt.ylabel("sum of distance")
+# plt.legend()
+# plt.show()
+# outputdict, pivots, num_of_iter, sumofdist = output
+# for key, value in outputdict.items():
+# 	print("Cluster No.{} has blog {}".format(key, value))
+# print("Went through {} iterations.".format(num_of_iter))
 
